fd702189a2a0f9d5dea91899d9a65b53185a813e
Panda88CO <christian.olgaard@gmail.com>
2024-02-02 16:28:41 -0800

    Update NetatmoWeather.py

--- NetatmoWeather.py
+++ NetatmoWeather.py
@@ -216,7 +216,7 @@ class NetatmoWeather (NetatmoCloud):
                         logging.debug(self.cloud_data[home_id][module_type][module_adr])
                         logging.debug(self.cloud_data)
                         cloud_mod_adr_data = self.cloud_data[home_id][module_type][module_adr]
-                        logging.debug(cloud_mod_adr_data['time_utc'])
+                        logging.debug(cloud_mod_adr_data['time_utc'], len(cloud_mod_adr_data ))
                         for dat in cloud_mod_adr_data:
                             logging.debug('for loop cloud only ONLY { }'.format(dat))
                             data_str = self.merge_data_str(dat)
