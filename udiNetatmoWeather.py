b52702d61c0f7cd196b806948d28e1983b06847c
Panda88CO <christian.olgaard@gmail.com>
2024-02-26 14:48:30 -0800

    Update udiNetatmoWeather.py

--- udiNetatmoWeather.py
+++ udiNetatmoWeather.py
@@ -26,7 +26,7 @@ from NetatmoWeather import NetatmoWeather
 from  udiNetatmoWeatherMain import udiNetatmoWeatherMain
 #from nodes.controller import Controller
 #from udi_interface import logging, Custom, Interface
-version = '0.1.0'
+version = '0.1.1'
 
 #polyglot = None
 #myNetatmo = None
